476424f178cc59d5a2b36468bd7bbf6062286106
Benjamin <ben@zarktech.com>
2018-07-30 11:08:55 -0700

    More stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/layouts/Home';
+import About from './components/layouts/About';
 
+ 
 class App extends Component {
-    render() {
-        return (
-          <Home/>
-        );
-    }
+  render() {
+    return (
+        <BrowserRouter>
+            <div>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+            </div>
+        </BrowserRouter>
+       );
+  }
 }
 
 export default App;
